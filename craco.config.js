e3cfc6c39253d208586366f9b75150f1e06a625d
develden <wqjasabim@mail.ru>
2025-02-06 18:36:10 +0300

    fix: удалено свойство extensionsToTreatAsEsm из конфигурации Jest

--- craco.config.js
+++ craco.config.js
@@ -4,14 +4,10 @@ module.exports = {
       // Создаем копию конфигурации
       const newJestConfig = { ...jestConfig };
 
-      // Если extensionsToTreatAsEsm задан и представляет массив,
-      // удаляем из него значение ".js"
-      if (newJestConfig.extensionsToTreatAsEsm && Array.isArray(newJestConfig.extensionsToTreatAsEsm)) {
-        newJestConfig.extensionsToTreatAsEsm = newJestConfig.extensionsToTreatAsEsm.filter(ext => ext !== '.js');
-        // Если после фильтрации массив пуст, удаляем всё свойство
-        if (newJestConfig.extensionsToTreatAsEsm.length === 0) {
-          delete newJestConfig.extensionsToTreatAsEsm;
-        }
+      // Вместо фильтрации удаляем ключ extensionsToTreatAsEsm полностью,
+      // чтобы избежать ошибки, что '.js' всегда inferred.
+      if (newJestConfig.extensionsToTreatAsEsm) {
+        delete newJestConfig.extensionsToTreatAsEsm;
       }
 
       // Используем паттерн, который позволяет обрабатывать модуль axios из node_modules
